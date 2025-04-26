54f859ea1fa26517579b9f52a022905c4f16213b
Hajime Kawahara <divrot@gmail.com>
2025-04-26 16:55:36 +0900

    added gibbs

--- src/exogibbs/equilibrium/thermochem.py
+++ src/exogibbs/equilibrium/thermochem.py
@@ -1,4 +1,6 @@
 from exogibbs.io.load_data import load_molname
+from exogibbs.io.load_data import load_JANAF_molecules
+from exogibbs.io.load_data import load_formula_matrix
 
 
 class ThermoChem:
@@ -9,14 +11,20 @@ class ThermoChem:
     def __init__(self):
         pass
 
-    def setting(self, path_JANAF_data):
-        """
-        Set the molecules, elements, 
+    def set_equations(self, path_JANAF_data):
+        """Set the molecules, elements, chemical_data (eq_setting in thermochemical_equilibrium package)
         """
         self.df_molname = load_molname()
+        self.chemical_data = load_JANAF_molecules(
+            self.df_molname,
+            path_JANAF_data,
+        )
+        self.formula_matrix = load_formula_matrix()
 
-
+    def set_initial_values(self):
+        """Set the initial values for the equilibrium calculation.
+        """
+        return
 
 def thermochemical_equilibrium(pressures, temperatures):
     return None
-
