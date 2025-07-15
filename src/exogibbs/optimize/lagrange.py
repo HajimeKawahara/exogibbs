05f4df8bd6020e489f4160624976cc4a4cc5d6fe
Hajime Kawahara <divrot@gmail.com>
2025-07-15 10:04:03 +0900

    added test for lagrange

--- src/exogibbs/optimize/lagrange.py
+++ src/exogibbs/optimize/lagrange.py
@@ -1,5 +1,43 @@
-def lagrange_newton(T, P, T_table, mu_table, x0, formula_matrix, b, stepsize=1e-2, num_steps=1000
+import jax.numpy as jnp
+from jax import custom_vjp
+from jax.lax import while_loop
+
+
+# def lagrange_newton(T, P, T_table, mu_table, x0, formula_matrix, b, stepsize=1e-2, num_steps=1000):
+
+
+def _A_diagn_At(number_density_vector, formula_matrix):
+    return jnp.einsum(
+        "ik,k,jk->ij", formula_matrix, number_density_vector, formula_matrix
+    )
+
+
+# @custom_vjp
+def solve_gibbs_iteration_equations(
+    nk,
+    ntotk,
+    A,
+    b
+    gk
 ):
+    An = A @ nk
+    AnAt = _A_diagn_At(nk, A)
+    Angk = A @ (gk*nk) 
+    
+
+
+def interation():
+    def cond_fun(carry):
+        E_prev, E = carry
+        return jnp.abs(E - E_prev) > 1e-6
+
+    def body_fun(carry):
+        _, E = carry
+        E_new = E - f(E, e, M) / dfdE(E, e, M)
+        return E, E_new
+
+    _, E_star = while_loop(cond_fun, body_fun, (jnp.inf, Eini))
+    return E_star
 
 
 if __name__ == "__main__":
@@ -14,10 +52,8 @@ if __name__ == "__main__":
 
     config.update("jax_enable_x64", True)
 
-
     ref = pd.read_csv("yk.list", header=None, sep=",").values[0]
     print("ref", ref.shape)
-
 
     path = get_data_filepath(DEFAULT_JANAF_GIBBS_MATRICES)
     gibbs_matrices = np.load(path, allow_pickle=True)["arr_0"].item()
@@ -25,11 +61,9 @@ if __name__ == "__main__":
     molecules, T_table, mu_table, grid_lens = extract_and_pad_gibbs_data(gibbs_matrices)
     # checks using the results of Kawashima's code.
 
-
-
     npath = get_data_filepath(NUMBER_OF_SPECIES_SAMPLE)
-    
-    tip = 1.e-20
+
+    tip = 1.0e-20
     number_of_species_init = pd.read_csv(npath, header=None, sep=",").values[0] + tip
     T = 500.0
     P = 10.0
@@ -37,7 +71,7 @@ if __name__ == "__main__":
     formula_matrix = load_formula_matrix()
 
     b = formula_matrix @ number_of_species_init
-    #print("init Gibbs:",computes_total_gibbs_energy(number_of_species_init, T, P, T_table, mu_table, Pref=1.0))
+    # print("init Gibbs:",computes_total_gibbs_energy(number_of_species_init, T, P, T_table, mu_table, Pref=1.0))
 
     x_opt = optimize_lagrange(
         T,
@@ -59,6 +93,7 @@ if __name__ == "__main__":
     print(x_opt)
 
     import matplotlib.pyplot as plt
+
     plt.plot(number_of_species_init, "^")
     plt.plot(x_opt, "o")
     plt.plot(ref, "x")
