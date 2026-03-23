2d80b9a265de3cc2116256f697931e42de1f76d6
Hajime Kawahara <divrot@gmail.com>
2026-03-23 11:35:04 +0900

    version: align runtime version with setuptools_scm

--- src/exogibbs/__init__.py
+++ src/exogibbs/__init__.py
@@ -1,8 +1,16 @@
-try:
-    from .ExoGibbs_version import __version__
-except Exception:
+from importlib import import_module
+from importlib.metadata import version as _pkg_version
+
+
+def _load_generated_version() -> str:
+    return import_module(".exogibbs_version", __name__).__version__
+
+
+def _load_version() -> str:
     try:
-        from importlib.metadata import version as _pkg_version
-        __version__ = _pkg_version("ExoGibbs")
-    except Exception:
-        __version__ = "0.0.0.dev0"
+        return _load_generated_version()
+    except (ImportError, AttributeError):
+        return _pkg_version("exogibbs")
+
+
+__version__ = _load_version()
